9a87e8f79868e825e2a14147e89c0b83cd4f0426
aharita <aharita@nearsoft.com>
2012-07-28 21:05:48 -0700

    initial commit

---
+++ MagicLogging.Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MagicLogging.Demo
+{
+    [MagicLogging("Id", "Name")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
+    [MagicLogging]
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User
+            {
+                Id = 412,
+                Name = "Alfonso Harita",
+                Password = "secret"
+            };
+
+            var achievement = new Achievement
+            {
+                Id = 24917,
+                User = user,
+                Description = "Learn AOP"
+            };
+
+            var result = MagicLoggingAttribute.MagicLog(achievement);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MagicLogging/MagicLoggingAttribute.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagicLogging
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class MagicLoggingAttribute : Attribute
+    {
+        public List<string> PropertiesToLog = new List<string>();
+
+        public MagicLoggingAttribute()
+        {
+
+        }
+
+        public MagicLoggingAttribute(params string[] properties)
+        {
+            PropertiesToLog.AddRange(properties);
+        }
+
+        public static string MagicLog(object obj)
+        {
+            var result = MagicLogger(obj);
+            return string.Join("|", result);
+        }
+
+        private static IEnumerable<string> MagicLogger(object obj, string parentType = "")
+        {
+            var messageList = new List<string>();
+
+            if (obj == null) return messageList;
+
+            var type = obj.GetType();
+            var attributes = GetCustomAttributes(type);
+
+            if (String.IsNullOrEmpty(parentType))
+            {
+                parentType = type.Name;
+            }
+            else
+            {
+                parentType = String.Format("{0}.{1}", parentType, type.Name);
+            }
+
+            foreach (var attr in attributes)
+            {
+                if (attr is MagicLoggingAttribute)
+                {
+                    var logAttrInstance = ((MagicLoggingAttribute)attr);
+                    var properties = type.GetProperties();
+
+                    foreach (var property in properties)
+                    {
+                        if (logAttrInstance.PropertiesToLog.Count != 0 && !logAttrInstance.PropertiesToLog.Contains(property.Name))
+                        {
+                            continue;
+                        }
+
+                        bool suppress = false;
+                        var val = property.GetValue(obj, null);
+                        if (val != null)
+                        {
+                            messageList.AddRange((MagicLogger(val, parentType)));
+
+                            var t = val.GetType();
+                            var a = GetCustomAttributes(t);
+
+                            foreach (var littleAttr in a)
+                            {
+                                if (littleAttr is MagicLoggingAttribute)
+                                {
+                                    suppress = true;
+                                }
+                            }
+                        }
+
+                        string logMessage = String.Format("{0}.{1}={2}", parentType, property.Name, val);
+                        if (!suppress)
+                        {
+                            messageList.Add(logMessage);
+                        }
+                    }
+                }
+            }
+
+            return messageList;
+        }
+    }
+}
